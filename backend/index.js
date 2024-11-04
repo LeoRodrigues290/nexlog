c44b6ae2b73a1fb2cfcbc22622f065b42ed209ab
Léo <leo.rodriguessc@gmail.com>
2024-11-04 18:29:42 -0300

    Connect Mongo DB and install Cors

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const Log = require("./models/log");
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
@@ -11,12 +12,12 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => console.log(err));
+
 
 // Define rota para salvar logs
 app.post("/api/logs", async (req, res) => {
@@ -29,8 +30,6 @@ app.post("/api/logs", async (req, res) => {
         res.status(400).json({ error: err.message });
     }
 });
-
-const Log = require("./models/Log");
 
 // Rota para buscar logs com filtro
 app.get("/api/logs", async (req, res) => {
